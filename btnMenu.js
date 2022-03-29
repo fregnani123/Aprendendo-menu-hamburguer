4d2501540338dff7ffb3abb95443ba18967b1fd9
Fabiano <99106631+fregnani123@users.noreply.github.com>
2022-03-29 19:12:37 -0300

    Initial commit

---
+++ btnMenu.js
@@ -0,0 +1,20 @@
+
+
+function toggleMenu(){
+  const nav = document.querySelector('nav#nav');
+  nav.classList.toggle('active')}
+
+
+
+
+
+
+
+
+
+/* Temos opção de criar o evento aqui direto no js pelo addEventListener ou criar pelo HTML usando onClick ('toggleMenu()) 
+
+btnMobile.addEventListener ('click', toggleMenu); */
+
+ 
+  
