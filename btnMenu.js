da1ce9c9849bebc9d2e5619adee67a22ba620b6d
Fabiano <99106631+fregnani123@users.noreply.github.com>
2022-03-31 18:18:23 -0300

    atualizar

--- btnMenu.js
+++ btnMenu.js
@@ -1,21 +1,15 @@
+const btnMobile = document.querySelector('button#btn-mobile')
 
 
-
-function toggleMenu(){
+function toggleMenu(event){
+  if (event.type === 'touchstart') event.preventDefault()
   const nav = document.querySelector('nav#nav');
   nav.classList.toggle('active')}
 
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu); /* touchstart: previne o delay na hora do click no mobile... para funcionar tem que verificar if ...evnt.type....
+
+opções de acessibilidade não foi adicionados ainda.
 
 
-
-
-
-
-
-
-/* Temos opção de criar o evento aqui direto no js pelo addEventListener ou criar pelo HTML usando onClick ('toggleMenu()) 
-
-btnMobile.addEventListener ('click', toggleMenu); */
-
- 
-  
+/* Temos opção de criar o evento aqui direto no js pelo addEventListener ou criar pelo HTML usando onClick = ('toggleMenu()) direto na tag... os 2 funcionam.*/
